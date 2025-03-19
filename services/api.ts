9c066224a6d70a84c4efc84b944bf75b8325b3d8
GlaDrancE <ayushr16060@gmail.com>
2025-03-19 23:01:27 +0530

    Updating api.ts for new microservices

--- services/api.ts
+++ services/api.ts
@@ -2,12 +2,19 @@ import axios from "axios";
 import { Agent, Client } from "../types";
 
 const api = axios.create({
-    baseURL: 'https://trutis-backend.onrender.com/api',
+    baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:3000/api',
+
 });
+
+const authApi = axios.create({
+    baseURL: import.meta.env.VITE_AUTH_URL || 'http://localhost:3000/auth'
+})
+
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem("token");
+        const authProvider = JSON.parse(localStorage.getItem("auth-storage") as string)?.state.authProvider
 
         if (!config.headers) {
             config.headers = new axios.AxiosHeaders();
@@ -15,7 +22,12 @@ api.interceptors.request.use(
 
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
+            config.data = {
+                ...config.data,
+                authProvider: authProvider
+            }
         }
+
 
         return config;
     },
@@ -23,19 +35,41 @@ api.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+const getRefreshTokenFromCookie = () => {
+    const cookies = document.cookie.split(';');
+    const refreshTokenCookie = cookies.find(cookie => cookie.trim().startsWith('refreshToken='));
+    return refreshTokenCookie ? refreshTokenCookie.split('=')[1].trim() : null;
+};
+api.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config;
+        if (error.response.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            try {
+                const { data } = await axios.post('http://your-api-url/token/refresh', {
+                    refreshToken: getRefreshTokenFromCookie(),
+                });
+                localStorage.setItem("token", data.accessToken)
+                originalRequest.headers['Authorization'] = `Bearer ${data.accessToken}`;
+                return api(originalRequest);
+            } catch (err) {
+                // Handle refresh token expiration (e.g., redirect to login)
+                localStorage.removeItem("token")
+                localStorage.removeItem("refreshToken")
+                window.location.href = "/login"
+            }
+        }
+        return Promise.reject(error);
+    }
+);
 
-// Auth
-export const login = (email: string, password: string) =>
-    api.post("/auth/admin/login", { email, password });
 
-export const register = (email: string, password: string) =>
-    api.post("/auth/admin/signup", { email, password });
 
-export const sendResetPasswordEmail = (email: string) =>
-    api.post("/client/sendresetpassword", { email });
 
-export const resetPassword = (token: string, password: string) =>
-    api.post("/client/resetpassword", { token, password });
+export const loginAgent = (email: string, password: string) =>
+    authApi.post("/agent/login", { email, password });
+
 
 // Agents
 export const getAgents = () => api.get("/agents");
@@ -48,8 +82,6 @@ export const updateAgent = (id: string, data: Partial<Agent>) =>
 
 export const deleteAgent = (id: string) => api.delete(`/agents/${id}`);
 
-export const loginAgent = (email: string, password: string) =>
-    api.post("/auth/agent/login", { email, password });
 
 export const getAgentClients = (agentId: string) => api.get(`/agent-clients/${agentId}`);
 
@@ -60,7 +92,7 @@ export const updateAgentStatus = (agentId: string, newStatus: boolean) =>
 
 export const searchClient = (publicKey: string) => api.post(`/agents/client`, { publicKey });
 
-export const linkQRCode = (publicKey: string, QRId: string, agentId: string) =>
+export const linkQRCode = (publicKey: string, QRId: string, agentId?: string) =>
     api.post("/agents/linkQRCode", { publicKey, QRId, agentId });
 
 // Clients
@@ -68,14 +100,6 @@ export const getClients = () => api.get("/clients");
 
 export const getClient = (client_id: string) => api.get(`/client/${client_id}`);
 
-export const loginClient = (email: string, password: string) =>
-    api.post("/auth/client/login", { email, password });
-
-export const createClient = (data: Omit<Client, "id" | "created_at">): Promise<any> => {
-    return api.post("/auth/clients/register", data, {
-        headers: { "Content-Type": "multipart/form-data" },
-    });
-};
 export const createClientPublicKey = (clientId: string) => api.post("/client/create-public-key", { clientId });
 export const createGoogleClient = (data: Omit<Client, "id" | "created_at">): Promise<any> => {
     return api.post("/auth/clients/google/register", data);
@@ -86,9 +110,16 @@ export const verifyClientOtp = (data: string) => {
 };
 
 export const updateClient = (id: string, data: Partial<Client>) =>
-    api.put(`/clients/${id}`, data);
+    api.put(`/clients/${id}`, data, {
+        headers: { "Content-Type": "multipart/form-data" },
+    });
+
 
 export const deleteClient = (id: string) => api.delete(`/clients/${id}`);
+export const uploadToCloudinary = (data: { file: File, upload_preset: string }) => api.post("https://api.cloudinary.com/v1_1/doahncdjq/image/upload", { data })
+// TODO: Need to create endpoint for google bucket upload
+
+
 
 // Staff
 export const updateStaff = (data: { client_id: string; staff_id?: string; staff_password?: string; staffStatus?: boolean }) =>
@@ -104,13 +135,9 @@ export const getQrId = (token: string) => {
     return api.post("/client/getqrid", { token });
 };
 
-// Payment Logs
-export const getPaymentLogs = () => api.get("/payment-logs");
 
-export const getPlans = () => api.get("/clients/subscription-plans");
-export const createCheckoutSession = (lookup_key: string, clientId: string) => api.post("/payments/create-checkout-session", { lookup_key, clientId });
-export const verifyPaymentAndStore = (session_id: string) => api.post("/payments/verify", { session_id });
-export const portalSession = (customerId: string) => api.post("/payment/create-portal-session", { customerId });
+
+
 // OTP
 export const verifyOtp = (data: { email: string; otp: string }) =>
     api.post("/client/verify-otp", data);
@@ -120,17 +147,20 @@ export const generateOtp = (email: string) =>
 
 // Coupons
 export const getCoupons = (id: string) => api.get(`/forms/get-coupons/${id}`);
-export const fetchCustomerFromCoupon = (code: string) => api.post(`/clients/verify`, { code });
 export const generateCoupon = (data: { qr_id: string, code: string, email: string, name: string, phone: string, DOB: string, ratings: number | null, reviewImage: File | null }) => api.post(`/coupon`, data, {
     headers: { "Content-Type": "multipart/form-data" },
 });
 
+// Customers
+export const fetchCustomerFromCoupon = (code: string) => api.post(`/coupon/verify`, { code });
+export const getCustomers = (id: string) => api.get(`/forms/get-customers/${id}`);
+export const getCustomer = (id: string) => api.get(`/forms/get-customer/${id}`)
+export const fetchCustomerFromCouponID = (couponId: string) => api.post(`/coupon/getcustomer`, { couponId });
+export const fetchReviewsFromClientId = (clientId: string) => api.post(`/clients/reviews`, { clientId });
 
 // Admin
 export const getStats = () => api.get("/admin/getStats");
 
-// Payments
-export const createProducts = (client_id: string) => api.post("/payments/create-products", { client_id });
 
 // Forms
 export const getClientFromQR = (qr_id: string) => api.post("/forms/get-client", { qr_id: qr_id });
